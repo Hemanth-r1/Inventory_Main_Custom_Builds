5a807abf8edc2d05c8da20aa29bea7bfa047646b
RADEON_POLARIS <hemanth.r8892@gmail.com>
2021-05-22 22:34:27 +0530

    almost completed

--- app/src/main/java/in/hr/android/inventorymain/MainActivity.java
+++ app/src/main/java/in/hr/android/inventorymain/MainActivity.java
@@ -2,7 +2,6 @@ package in.hr.android.inventorymain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Canvas;
@@ -14,12 +13,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -32,9 +25,15 @@ import ir.androidexception.datatable.DataTable;
 import ir.androidexception.datatable.model.DataTableHeader;
 import ir.androidexception.datatable.model.DataTableRow;
 
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton floatingAddButton, floatingNewButton;
+    Convert_Quotation_To_PDF_Helper_SQL helperSQL;
     Button printButton;
     EditText editText;
     DataTable dataTable;
@@ -57,19 +56,18 @@ public class MainActivity extends AppCompatActivity {
         floatingAddButton = findViewById(R.id.floatingAddQuoteButton);
         floatingNewButton = findViewById(R.id.floatingAddBillButton);
 
-        DataTableHeader header = new DataTableHeader.Builder().item("Bill No", 5)
-                .item("Bill Name", 5)
+        helperSQL = new Convert_Quotation_To_PDF_Helper_SQL(this);
+        database = helperSQL.getWritableDatabase();
+
+        DataTableHeader header = new DataTableHeader.Builder().item("Invoice No", 5)
+                .item("Customer Name", 5)
                 .item("Date", 5)
-                .item("Mobile No", 5)
-                .item("Amount", 5)
+                .item("mobileNo", 5)
+                .item("amount", 5)
                 .build();
 
-        dataTable.setHeader(header);
-        dataTable.setRows(rows);
-        dataTable.inflate(this);
-/*
-        String[] column = {"billNo", "billName", "date", "mobileNo", "amount"};
-        Cursor cursor = database.query("BillTable", column, null, null, null, null, null);
+        String[] column = {"quoteNo", "date", "mobileNo", "amount"};
+        Cursor cursor = database.query("QuoteTable", column, null, null, null, null, null);
 
         for (int i = 0; i < cursor.getCount(); i++) {
 
@@ -81,10 +79,13 @@ public class MainActivity extends AppCompatActivity {
                     .value(String.valueOf(cursor.getInt(3)))
                     .value(String.valueOf(cursor.getInt(4)))
                     .build();
-
             rows.add(row);
-           // cursor.close();
-        } */
+        }
+
+        dataTable.setHeader(header);
+        dataTable.setRows(rows);
+        dataTable.inflate(this);
+
         floatingAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -105,123 +106,114 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void printSelectedInvoice(View view) {
-        printButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PdfDocument pdfDocument = null;
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
-                    pdfDocument = new PdfDocument();
 
-                    Paint paint = new Paint();
+        PdfDocument pdfDocument = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            pdfDocument = new PdfDocument();
 
-                    int invoiceForPDF = Integer.parseInt(editText.toString());
+            Paint paint = new Paint();
 
-                    Cursor cursor = database.rawQuery("SELECT * FROM PdfTable where invoiceNo = " + invoiceForPDF, null);
-                    cursor.moveToNext();
+            int invoiceForPDF = Integer.parseInt(editText.toString());
+            Cursor cursor = database.rawQuery("SELECT * FROM QuoteTable where invoiceNo = " + invoiceForPDF, null);
+            cursor.moveToNext();
 
-                    PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(1000, 900, 1).create();
-                    PdfDocument.Page page = pdfDocument.startPage(pageInfo);
+            PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(1000, 900, 1).create();
+            PdfDocument.Page page = pdfDocument.startPage(pageInfo);
 
-                    Canvas canvas = page.getCanvas();
+            Canvas canvas = page.getCanvas();
 
-                    //first text
-                    paint.setTextSize(80);
-                    canvas.drawText("Custom Builds", 30, 80, paint);
+            //first text
+            paint.setTextSize(80);
+            canvas.drawText("Custom Builds", 30, 80, paint);
 
-                    //second text
-                    paint.setTextSize(30);
-                    canvas.drawText("#21, Rajeev Gandji Nagar, Nandini Layout, bengaluru 96", 30, 120, paint);
+            //second text
+            paint.setTextSize(30);
+            canvas.drawText("#21, Rajeev Gandji Nagar, Nandini Layout, bengaluru 96", 30, 120, paint);
 
-                    //
-                    paint.setTextAlign(Paint.Align.RIGHT);
-                    canvas.drawText("Invoice Number", canvas.getWidth() - 40, 80, paint);
-                    canvas.drawText(String.valueOf(cursor.getInt(0)), canvas.getWidth() - 40, 80, paint);
-                    paint.setTextAlign(Paint.Align.LEFT);
+            //
+            paint.setTextAlign(Paint.Align.RIGHT);
+            canvas.drawText("Invoice Number", canvas.getWidth() - 40, 80, paint);
+            canvas.drawText(String.valueOf(cursor.getInt(0)), canvas.getWidth() - 40, 80, paint);
+            paint.setTextAlign(Paint.Align.LEFT);
 
-                    paint.setColor(Color.rgb(150, 150, 150));
-                    canvas.drawRect(30, 150, canvas.getWidth() - 30, 160, paint);
+            paint.setColor(Color.rgb(150, 150, 150));
+            canvas.drawRect(30, 150, canvas.getWidth() - 30, 160, paint);
 
-                    paint.setColor(Color.BLACK);
-                    canvas.drawText("Date", 50, 200, paint);
-                    canvas.drawText(dateFormat.format(cursor.getLong(3)), 250, 200, paint);
+            paint.setColor(Color.BLACK);
+            canvas.drawText("Date", 50, 200, paint);
+            canvas.drawText(dateFormat.format(cursor.getLong(3)), 250, 200, paint);
 
-                    canvas.drawText("Time", 620, 200, paint);
-                    paint.setTextAlign(Paint.Align.RIGHT);
-                    canvas.drawText(timeFormat.format(cursor.getLong(3)), canvas.getWidth() - 40, 200, paint);
-                    paint.setTextAlign(Paint.Align.LEFT);
+            canvas.drawText("Time", 620, 200, paint);
+            paint.setTextAlign(Paint.Align.RIGHT);
+            canvas.drawText(timeFormat.format(cursor.getLong(3)), canvas.getWidth() - 40, 200, paint);
+            paint.setTextAlign(Paint.Align.LEFT);
 
-                    paint.setColor(Color.rgb(150, 150, 150));
-                    canvas.drawRect(30, 250, 250, 300, paint);
+            paint.setColor(Color.rgb(150, 150, 150));
+            canvas.drawRect(30, 250, 250, 300, paint);
 
-                    paint.setColor(Color.WHITE);
-                    canvas.drawText("Bill to", 50, 285, paint);
+            paint.setColor(Color.WHITE);
+            canvas.drawText("Bill to", 50, 285, paint);
 
-                    paint.setColor(Color.BLACK);
-                    canvas.drawText("Customer Name:", 30, 350, paint);
-                    canvas.drawText(cursor.getString(1), 280, 350, paint);
+            paint.setColor(Color.BLACK);
+            canvas.drawText("Customer Name:", 30, 350, paint);
+            canvas.drawText(cursor.getString(1), 280, 350, paint);
 
-                    canvas.drawText("Phone", 620, 350, paint);
-                    paint.setTextAlign(Paint.Align.RIGHT);
-                    canvas.drawText(cursor.getString(2), canvas.getWidth() - 40, 350, paint);
-                    paint.setTextAlign(Paint.Align.LEFT);
+            canvas.drawText("Phone", 620, 350, paint);
+            paint.setTextAlign(Paint.Align.RIGHT);
+            canvas.drawText(cursor.getString(2), canvas.getWidth() - 40, 350, paint);
+            paint.setTextAlign(Paint.Align.LEFT);
 
-                    // create rectangular bar
-                    paint.setColor(Color.rgb(150, 150, 150));
-                    canvas.drawRect(30, 400, canvas.getWidth() - 40, 450, paint);
+            // create rectangular bar
+            paint.setColor(Color.rgb(150, 150, 150));
+            canvas.drawRect(30, 400, canvas.getWidth() - 40, 450, paint);
 
-                    paint.setColor(Color.WHITE);
-                    canvas.drawText("Item", 50, 435, paint);
-                    canvas.drawText("Qty", 550, 435, paint);
-                    paint.setTextAlign(Paint.Align.RIGHT);
+            paint.setColor(Color.WHITE);
+            canvas.drawText("Item", 50, 435, paint);
+            canvas.drawText("Qty", 550, 435, paint);
+            paint.setTextAlign(Paint.Align.RIGHT);
 
-                    canvas.drawText("Amount", canvas.getWidth() - 40, 435, paint);
-                    paint.setTextAlign(Paint.Align.LEFT);
+            canvas.drawText("Amount", canvas.getWidth() - 40, 435, paint);
+            paint.setTextAlign(Paint.Align.LEFT);
 
-                    paint.setColor(Color.BLACK);
-                    canvas.drawText(cursor.getString(4), 50, 480, paint);
-                    canvas.drawText(String.valueOf(cursor.getInt(5)), 550, 480, paint);
-                    paint.setTextAlign(Paint.Align.LEFT);
-                    canvas.drawText(String.valueOf(cursor.getInt(6)), canvas.getWidth() - 40, 480, paint);
+            paint.setColor(Color.BLACK);
+            canvas.drawText(cursor.getString(4), 50, 480, paint);
+            canvas.drawText(String.valueOf(cursor.getInt(5)), 550, 480, paint);
+            paint.setTextAlign(Paint.Align.LEFT);
+            canvas.drawText(String.valueOf(cursor.getInt(6)), canvas.getWidth() - 40, 480, paint);
 
-                    paint.setColor(Color.rgb(150, 150, 150));
-                    canvas.drawRect(30, 550, canvas.getWidth() - 30, 560, paint);
+            paint.setColor(Color.rgb(150, 150, 150));
+            canvas.drawRect(30, 550, canvas.getWidth() - 30, 560, paint);
 
-                    paint.setColor(Color.BLACK);
-                    canvas.drawText("Sub Total", 550, 600, paint);
-                    canvas.drawText("Tax 4%", 550, 640, paint);
-                    paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+            paint.setColor(Color.BLACK);
+            canvas.drawText("Sub Total", 550, 600, paint);
+            canvas.drawText("Tax 4%", 550, 640, paint);
+            paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
 
-                    canvas.drawText("TOTAL", 550, 680, paint);
-                    paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+            canvas.drawText("TOTAL", 550, 680, paint);
+            paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
 
-                    paint.setTextAlign(Paint.Align.RIGHT);
-                    canvas.drawText(String.valueOf(cursor.getInt(6)), 970, 600, paint);
-                    canvas.drawText(String.valueOf(cursor.getInt(6) * 4 / 100), 970, 640, paint);
-                    paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
-                    canvas.drawText(String.valueOf(cursor.getInt(6) + (cursor.getInt(6) * 4 / 100)), 970, 680, paint);
+            paint.setTextAlign(Paint.Align.RIGHT);
+            canvas.drawText(String.valueOf(cursor.getInt(6)), 970, 600, paint);
+            canvas.drawText(String.valueOf(cursor.getInt(6) * 4 / 100), 970, 640, paint);
+            paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+            canvas.drawText(String.valueOf(cursor.getInt(6) + (cursor.getInt(6) * 4 / 100)), 970, 680, paint);
 
-                    paint.setTextAlign(Paint.Align.LEFT);
-                    canvas.drawText("Make all check payable to \"Custom Builds\"", 30, 800, paint);
-                    paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+            paint.setTextAlign(Paint.Align.LEFT);
+            canvas.drawText("Make all check payable to \"Custom Builds\"", 30, 800, paint);
+            paint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
 
-                    canvas.drawText("Thank you very much", 30, 840, paint);
-                    pdfDocument.finishPage(page);
+            canvas.drawText("Thank you very much", 30, 840, paint);
+            pdfDocument.finishPage(page);
+            cursor.close();
 
-                    File file = new File(MainActivity.this.getExternalFilesDir("/"), cursor.getInt(0) + "_CustomBuilds.pdf");
+            File file = new File(MainActivity.this.getExternalFilesDir("/"), cursor.getInt(0) + "_CustomBuilds.pdf");
 
-                    try {
-                        pdfDocument.writeTo(new FileOutputStream(file));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    pdfDocument.close();
-                }
+            try {
+                pdfDocument.writeTo(new FileOutputStream(file));
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        });
+            pdfDocument.close();
+        }
     }
-
- /*   public void firebase(View view) {
-        Intent intent = new Intent(this, FirebaseDatabaseActivity.class);
-        startActivity(intent);
-    }*/
 }
